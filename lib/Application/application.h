60014fe4fa5489345d9c6ca5a1405972030368ba
amrpomerantz <culturuse@gmail.com>
2024-05-12 16:07:29 +0200

    refactor: Modify structure to follow conventional architecture
    Comes with the addition of an application c and h file that initializes the necessary controllers and moves code that was originally in main into that layer.

---
+++ lib/Application/application.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void application_init();
+void application_run();
--- src/water_ec.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <inttypes.h>
-
-void water_ec_init(void);
-
-/// @brief Measures the electrical conductivity of the water in microsiemens per centimeter.
-uint16_t water_ec_measure(void);
