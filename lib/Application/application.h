8fb69b4169f58a0e1835ad18207ab73771fdd103
MichalSliwaSE <112940119+MichalSliwaSE@users.noreply.github.com>
2024-05-16 09:06:46 +0200

    feature: add ECC key generation

--- lib/Application/application.h
+++ lib/Application/application.h
@@ -1,4 +1,16 @@
 #pragma once
+#include "uECC.h"
+#include "aes.h"
+#include "sha256.h"
 
 void application_init();
 void application_run();
+
+// Define key and secret buffers
+extern uint8_t cloud_public_key[64];
+extern uint8_t cloud_private_key[32];
+extern uint8_t target_public_key[64];
+extern uint8_t target_private_key[32];
+extern uint8_t secret_target[32];
+extern uint8_t secret_cloud[32];
+extern uint8_t aes_key[32];
